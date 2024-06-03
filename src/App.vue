23b639101e12f1c42060a9130337a6b94c73b27f
YCHE0392 <stanpoldark@gmail.com>
2024-06-03 19:32:30 +1000

    update

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+import dashboard from './components/Dashboard.vue';
+</script>
+
+<template>
+  <main>
+    <authenticator  :sign-up-attributes="['given_name', 'family_name']">
+      <template v-slot="{ user, signOut }">
+        <dashboard />
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </authenticator>
+  </main>
+</template>
+
---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import UploadView from '../components/UploadImage.vue'
+import QueryView from '../components/TagQuery.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: 'upload',
+      component: UploadView
+    },
+    {
+      path: '/query',
+      component: QueryView
+    }
+  ]
+})
+
+export default router
