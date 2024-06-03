e10655a13ae161099431688152d6cfe7e567562d
YCHE0392 <stanpoldark@gmail.com>
2024-06-03 22:34:22 +1000

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: 'upload',
+      path: '/upload',
       component: UploadView
     },
     {
