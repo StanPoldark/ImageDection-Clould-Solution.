dacc428304a16acb43a053f114a5811042757a79
YCHE0392 <stanpoldark@gmail.com>
2024-06-05 14:22:05 +1000

    Update SNS.vue

--- src/components/SNS.vue
+++ src/components/SNS.vue
@@ -4,19 +4,19 @@
     <div class="tags-container">
       <div>
         <h3>Available Tags</h3>
-        <draggable v-model="items" group="people" @start="drag = true" @end="drag = false" item-key="id">
-      <template #item="{ element }">
-        <div>{{ element.name }}</div>
-      </template>
-    </draggable>
+        <draggable v-model="items" group="tags" @change="handleDrag" class="drag-area">
+          <template #item="{ element }">
+            <div class="tag-item">{{ element.name }}</div>
+          </template>
+        </draggable>
       </div>
       <div>
         <h3>Selected Tags</h3>
-        <draggable v-model="items" group="people" @start="drag = true" @end="drag = false" item-key="id">
-      <template #item="{ element }">
-        <div>{{ element.name }}</div>
-      </template>
-    </draggable>
+        <draggable v-model="selectedItems" group="tags" @change="handleDrag" class="drag-area">
+          <template #item="{ element }">
+            <div class="tag-item">{{ element.name }}</div>
+          </template>
+        </draggable>
       </div>
     </div>
   </div>
@@ -31,32 +31,58 @@ export default {
   components: {
     draggable,
   },
-  data() {
-    return {
-      items: [],
-      drag: false
+  setup() {
+    const items = ref([]);
+    const selectedItems = ref([]);
+    const drag = ref(false);
+
+    const fetchTags = () => {
+      axios.get('https://7m6gw11u0l.execute-api.us-east-1.amazonaws.com/prod/api/tags', {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        try {
+          const tags = JSON.parse(response.data.body);
+          items.value = tags.map((tag, index) => ({
+            name: tag,
+            id: index
+          }));
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+        }
+      })
+      .catch(error => console.error('Error fetching tags:', error));
     };
-  },
-  mounted() {
-    this.fetchTags();
-  },
-  methods: {
-  fetchTags() {
-    axios.get('https://7m6gw11u0l.execute-api.us-east-1.amazonaws.com/prod/api/tags', {
-      headers: {
-        'Content-Type': 'application/json'
+
+    const handleDrag = (event) => {
+      // This function is called whenever an item is dragged and dropped across lists
+      if (event.added) {
+        const newItem = event.added.element;
+        if (event.from === event.to) {
+          // Same list: no action needed
+        } else if (event.from === items.value) {
+          // From items to selectedItems
+          selectedItems.value.push(newItem);
+          items.value.splice(items.value.indexOf(newItem), 1);
+        } else {
+          // From selectedItems to items
+          items.value.push(newItem);
+          selectedItems.value.splice(selectedItems.value.indexOf(newItem), 1);
+        }
       }
-    })
-    .then(response => {
-      const tags = JSON.parse(response.data.body);
-      this.items = tags.map((tag, index) => ({
-        name: tag,
-        id: index
-      }));
-    })
-    .catch(error => console.error('Error fetching tags:', error));
+    };
+
+    onMounted(fetchTags);
+
+    return {
+      items,
+      selectedItems,
+      drag,
+      handleDrag
+    };
   }
-}
 }
 </script>
 
